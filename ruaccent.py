d9ac4a4bbd1707299d98b3863bb7aa0d154fa872
shakhovak <89096305+shakhovak@users.noreply.github.com>
2023-12-18 17:02:12 +0300

    updated with new dict

--- ruaccent.py
+++ ruaccent.py
@@ -8,11 +8,11 @@ from text_split import split_by_sentences
 
 class RUAccent:
     vowels = "аеёиоуыэюя"
+
     def __init__(self):
         self.omographs = None
         self.accents = None
         self.workdir = os.getcwd()
-
 
     def load(self, custom_accent=None, custom_omographs=None):
 
@@ -22,11 +22,11 @@ class RUAccent:
         if custom_accent is None:
             custom_accent = {}
 
-        self.omographs = json.load(open(join_path(self.workdir, "dictionaries", "omographs.json"), encoding='utf-8'))
+        self.omographs = json.load(open(join_path(self.workdir, "dictionaries", "file_omo.json"), encoding='utf-8'))
 
         self.omographs.update(custom_omographs)
 
-        self.accents = json.load(open(join_path(self.workdir, "dictionaries", "accents.json"), encoding='utf-8'))
+        self.accents = json.load(open(join_path(self.workdir, "dictionaries", "file_norm.json"), encoding='utf-8'))
 
         self.accents.update(custom_accent)
 
@@ -55,9 +55,7 @@ class RUAccent:
         outputs = []
         for sentence in sentences:
             text = self.split_by_words(sentence)
-            # processed_text = self._process_yo(text)
 
-            # processed_text = self._process_omographs(text)
             founded_omographs = self._process_omographs(text)
             omographs_list.extend(founded_omographs)
 
@@ -66,13 +64,11 @@ class RUAccent:
 
             processed_text = " ".join(processed_text)
             processed_text = self.delete_spaces_before_punc(processed_text)
-            # outputs.append(processed_text)
 
             accented_sentence.append(processed_text)
-            # " ".join(outputs)
 
         omographs_list = [f"{key}: {value}" for elem in omographs_list for key, value in elem.items()]
-        return accented_sentence, omographs_list, unknown_list
+        return accented_sentence, list(set(omographs_list)), list(set(unknown_list))
 
     def _process_yo(self, text):
         splitted_text = text
@@ -89,15 +85,9 @@ class RUAccent:
             variants = self.omographs.get(word)
             if variants:
                 founded_omographs.append(
-                    {word: variants}
+                    {word: self.omographs[word]["acc_variants"]}
                 )
 
-
-        # for omograph in founded_omographs:
-        #     splitted_text[omograph["position"]] = f"<w>{splitted_text[omograph['position']]}</w>"
-        #     cls = omograph["variants"][0]  # Just take the first variant from the dictionary
-        #     splitted_text[omograph["position"]] = cls
-        # return splitted_text
         return founded_omographs
 
     def _process_accent(self, text, founded_omographs):
@@ -105,23 +95,17 @@ class RUAccent:
         unknown_words = []
         for i, word in enumerate(splitted_text):
             stressed_word = self.accents.get(word, word)
-            if stressed_word == word:
-                # if len(word) > 4:
+
+            if stressed_word in [list(d.keys())[0] for d in founded_omographs]:
+                splitted_text[i] = word
+
+            elif stressed_word != word:
+                splitted_text[i] = stressed_word["accent"]
+
+            else:
                 if sum(word.count(vowel) for vowel in RUAccent.vowels) > 1:
                     unknown_words.append(word)
                 splitted_text[i] = word
-
-            elif stressed_word != word and word in [list(d.keys())[0] for d in founded_omographs]:
-                splitted_text[i] = word
-
-            else:
-                splitted_text[i] = stressed_word
-
-
-
-
-            # stressed_word = self.accents.get(word, word)
-            # splitted_text[i] = stressed_word
 
         return splitted_text, unknown_words
 
@@ -131,12 +115,11 @@ class RUAccent:
             text = text.replace(" " + char, char)
         return text
 
-
 # # Example usage:
 # ru_accent = RUAccent()
 # ru_accent.load()
 #
-# text_to_process = "В этом замке совершенно нет ни одного замка. Наверно я не буду ругаться с нига из-за этого сучонка"
+# text_to_process = "В этом замке совершенно нет ни одного замка. Наверно я не буду ругаться с нига нига нига из-за этого сучонка"
 # processed_text = ru_accent.process_all(text_to_process)
 #
 # print(processed_text)
--- web_interface.py
+++ web_interface.py
@@ -8,37 +8,83 @@ app = Flask(__name__)
 ru_accent = RUAccent()
 ru_accent.load()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/process', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/process", methods=["POST"])
 def process():
-    if request.method == 'POST':
-        input_text = request.form['input_text']
+    if request.method == "POST":
+        input_text = request.form["input_text"]
         processed_text = ru_accent.process_all(input_text)
 
         # Create three text files with the same content
 
-        file_name = 'accented_text.txt'
-        with open(file_name, 'w', encoding="utf-8") as file:
+        file_name = "accented_text.txt"
+        with open(file_name, "w", encoding="utf-8") as file:
             file.write(" ".join(processed_text[0]))
 
-        file_name = 'omographs.txt'
-        with open(file_name, 'w', encoding="utf-8") as file:
+        file_name = "omographs.txt"
+        with open(file_name, "w", encoding="utf-8") as file:
             file.write("\n".join(processed_text[1]))
 
-        file_name = 'unknown.txt'
-        with open(file_name, 'w', encoding="utf-8") as file:
+        file_name = "unknown.txt"
+        with open(file_name, "w", encoding="utf-8") as file:
             file.write("\n".join(processed_text[2]))
 
+        return render_template("result.html")
 
-        return render_template('result.html')
 
-@app.route('/download/<file_name>')
+@app.route("/upload", methods=["POST"])
+def upload():
+    # Check if the POST request has a file part
+    if "file" not in request.files:
+        return "No file part"
+
+    file = request.files["file"]
+
+    # If the user submits an empty form
+    if file.filename == "":
+        return "No selected file"
+
+    # Check if the file is a text file
+    if file and file.filename.endswith(".txt"):
+        # Save the uploaded file to the server (you might want to store it in a more secure way)
+        file.save(file.filename)
+
+        # Process the file content (replace this with your actual processing logic)
+        with open(file.filename, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        processed_text = ru_accent.process_all(content)
+
+        # Create three text files with the same content
+
+        file_name = "accented_text.txt"
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write(" ".join(processed_text[0]))
+
+        file_name = "omographs.txt"
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write("\n".join(processed_text[1]))
+
+        file_name = "unknown.txt"
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write("\n".join(processed_text[2]))
+
+        return render_template("result.html")
+
+    else:
+        return "Invalid file format. Please upload a text file."
+
+
+@app.route("/download/<file_name>")
 def download(file_name):
-    file_name = f'{file_name}'
-    return send_file(file_name, as_attachment=True, download_name=f'{file_name}')
+    file_name = f"{file_name}"
+    return send_file(file_name, as_attachment=True, download_name=f"{file_name}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=5001)
